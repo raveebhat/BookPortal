d2a38af4380bfc31bb8c9490679bcee906774f96
Raveendra Bhat <ravee@kubuntu.(none)>
2012-09-11 00:14:31 +0530

    Login bar added

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="css/custom.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet/less" type="text/css" href="less/bootstrap.less">
+        <link rel="stylesheet/less" type="text/css" href="less/variables.less">
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-            function test() {
-                echo'test';
-            }
-            test();
-        ?>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="navbar">
+                            <div class="navbar-inner">
+                                <a class="brand" href="#">BookPortal</a>
+                                <form class="navbar-form pull-right">
+                                    <input type="text" placeholder="Username">
+                                    <input type="text" placeholder="Password">
+                                    <div class="btn btn-small">Sign In</div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    
+                </div>
+            </div>
+        </div>
     </body>
 </html>
