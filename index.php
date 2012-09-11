49d3c9470e1bdf794e043d4eeb4a1c42658c2fb7
Raveendra Bhat <ravee@kubuntu.(none)>
2012-09-11 15:05:33 +0530

    Basic thumbnail layout design

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Book Portal</title>
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="css/custom.css" rel="stylesheet" type="text/css">
@@ -13,25 +13,193 @@
     </head>
     <body>
         <div class="wrapper">
-            <div class="container-fluid">
+            <div class="container-fluid"> 
                 <div class="row-fluid">
                     <div class="span12">
-                        <div class="navbar">
+                        <div class="navbar navbar-fixed-top navbar-inverse">
                             <div class="navbar-inner">
-                                <a class="brand" href="#">BookPortal</a>
-                                <form class="navbar-form pull-right">
-                                    <input type="text" placeholder="Username">
-                                    <input type="text" placeholder="Password">
-                                    <div class="btn btn-small">Sign In</div>
-                                </form>
+                                <div class="container">
+                                    <a class="brand" href="#">BookPortal</a>
+                                    <form class="navbar-form pull-right">
+                                        <input type="text" placeholder="Username" class="search-query">
+                                        <input type="text" placeholder="Password" class="search-query">
+                                        <input type="button" class="btn btn-small btn-info" value="Sign In">
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="mgap"></div>
             <div class="container-fluid">
                 <div class="row-fluid">
-                    
+                    <div class="span12">
+                        <ul class="thumbnails">
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="mgap"></div>
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <ul class="thumbnails">
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="mgap"></div>
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <ul class="thumbnails">
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                            <li class="span2">
+                                <div class="thumbnail">
+                                    <img src="http://placehold.it/300x200" alt="">
+                                    <div class="sgap"></div>
+                                    <input type="button" class="btn btn-small btn-primary" value="View">
+                                    <input type="button" class="btn btn-small btn-inverse pull-right" value="Download">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
