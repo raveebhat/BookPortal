098b7955061683f895a86251687a73516e2400f3
Arun-UB <arun.ub29@gmail.com>
2012-09-25 19:38:52 +0530

    Changes to uploader

--- uploader.php
+++ uploader.php
@@ -8,11 +8,17 @@ $bucket = 'book-bucket-' . strtolower($s3->key);
 
  $response = $s3->create_object($bucket,  $_FILES['uploadedfile']['name'], array(
     'fileUpload' => $_FILES['uploadedfile']['tmp_name'],
-     'storage' => AmazonS3::STORAGE_REDUCED
+     'storage' => AmazonS3::STORAGE_REDUCED,
+     'acl' => AmazonS3::ACL_PUBLIC,
+     'meta' => array(
+        'title'         => $_POST['title'],    // x-amz-meta-word
+        
+    ),
 ));
 
 
-var_dump($response->isOK());
+var_dump($_FILES);
+var_dump($response);
 
 
 
